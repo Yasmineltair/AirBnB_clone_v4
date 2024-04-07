c66323d6d55c6b2fad4698d6336bca7f1cd2b744
khaledolemy <khaledolemy@gmail.com>
2024-04-07 21:29:57 +0200

    reset sorted

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -2,10 +2,8 @@
 """ Starts a Flash Web Application """
 from models import storage
 from models.state import State
-from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from os import environ
 import uuid
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -30,11 +28,9 @@ def hbnb():
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
 
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
+    amenities = sorted(storage.all(Amenity).values(), key=lambda k: k.name)
 
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
+    places = sorted(storage.all(Place).values(), key=lambda k: k.name)
 
     return render_template('4-hbnb.html',
                            states=st_ct,

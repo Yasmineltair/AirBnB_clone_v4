2994d2e49d880a653906b14df1ce656192e28202
khaledolemy <khaledolemy@gmail.com>
2024-04-07 21:26:04 +0200

    lstrip&trim

--- web_dynamic/5-hbnb.py
+++ web_dynamic/5-hbnb.py
@@ -9,8 +9,8 @@ from os import environ
 import uuid
 from flask import Flask, render_template
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
